e7b3eac3de2e9be6872a0d7e4fd4fc28470b5189
Sue Anne <sueanneteo@gmail.com>
2014-02-02 23:51:33 +0800

    Completed Chapter 9

--- chap9.rb
+++ chap9.rb
@@ -53,6 +53,50 @@ puts(old_romans(1999))
 
 # New Roman numerals chapter 9 exercise
 
-def new_romans numbers
+def new_romans number
+
+thousands = (number / 1000)
+hundreds = (number % 1000 / 100)
+tens = (number % 100 / 10)
+ones = (number / 10)
+
+roman = 'M' * thousands
+
+if hundreds == 9
+	roman = roman + 'CM'
+elsif hundreds == 4
+	roman = roman + 'CD'
+else
+	roman = roman + 'D' * (number % 1000 / 500)
+	roman = roman + 'C' * (number % 500 / 100)
 end
+
+if tens == 9
+	roman = roman + 'XC'
+elsif tens == 4 
+	roman = roman + 'XL'
+else
+	roman = roman + 'L' * (number % 100 / 50)
+	roman = roman + 'X' * (number % 50 / 10)
+end
+
+if ones == 9
+	roman = roman + 'IX'
+elsif ones == 4
+	roman = roman + 'IV'
+else
+	roman = roman + 'V' * (number % 10 / 5)
+	roman = roman + 'I' * (number % 5 / 1)
+end
+
+roman
+end
+
+puts(new_romans(1999))
+
+
+
+
+
+
 
