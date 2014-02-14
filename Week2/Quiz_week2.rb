9a69dbdc3b7f28cd9c379446dffa708b9af63667
Sue Anne <sueanneteo@gmail.com>
2014-02-14 12:49:41 +0800

    Added Week2 Quiz Answers

---
+++ Week2/Quiz_week2.rb
@@ -0,0 +1,44 @@
+# Question no. 1
+
+puts "a = 1 is a local variable"
+puts "#... is a comment"
+puts "@a = 2 is an instance variable"
+puts "user = User.new is instantiating a new User object"
+puts "user.name is name getter of the object user"
+puts "user.name = 'Joe' is setting the name of the User object as Joe"
+
+# Question no. 2
+
+puts "a class mixes in a module by using the word 'include' to the module name right below the class name"
+
+# Question no. 3
+
+puts "the difference between a class variable and an instance variable is that an instance variable is not available to all of the class' descendents while class variables are. Class variables start with @@, instance variables start with @"
+
+# Question no.4
+
+puts "attr_accessor allows for both the getter and setter methods to be applied to the class"
+
+# Question no. 5
+
+puts "i would describe dog.some_method as setting the behaviour of the dog object"
+
+# Question no. 6
+
+puts "the difference between subclassing and mixing in modules is that modules cannot be instantiated. Subclasses inherit the behaviour of the parent class whereas modules have to be expressly mixed in through the word 'include'"
+
+# Question no. 7
+
+puts 
+  "def initialize(name)
+    @name = name
+  end"
+
+# Question no. 8
+
+puts "yes"
+
+# Question no. 9
+
+puts "pry"
+puts "testing different outcomes"
